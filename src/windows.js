7412c3916cc725b090d415c05840530e94337066
badvalou <52219672+badvalou@users.noreply.github.com>
2020-04-12 17:30:17 +0200

    Update windows.js

--- src/windows.js
+++ src/windows.js
@@ -17,7 +17,7 @@ const position = store.get('windowPosition');
 if (!position) {
   store.set('windowPosition', [0, 0]);
 }
-
+// creation de la page projet
 async function createProjectWindow() {
   projectWindow = new BrowserWindow({
     width: 1000,
@@ -28,12 +28,13 @@ async function createProjectWindow() {
       nodeIntegration: true
     }
   });
-  projectWindow.loadURL('file://' + __dirname + '/views/project.pug');
-  projectWindow.on('closed', function() {
+  projectWindow.loadURL('file://' + __dirname + '/views/project.pug'); // chargement du template
+  projectWindow.on('closed', function() { // evenement a la fermeture de la page projet
         projectWindow = null;
   });
 }
 
+// taille de la page a la derniere fermeture de l'application 
 const mainWindowWith = store.get('mainWindowWith');
 if(!mainWindowWith) {
     store.set('mainWindowWith', 800);
@@ -43,7 +44,7 @@ const mainWindowHeight = store.get('mainWindowHeight');
 if(!mainWindowHeight) {
     store.set('mainWindowHeight', 800);
 }
-
+// function createtion de la page principale
 async function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: mainWindowWith, height: mainWindowHeight,
@@ -55,7 +56,7 @@ async function createMainWindow() {
     }
   });
   mainWindow.loadURL('file://' + __dirname + '/views/index.pug');
-
+  // une équipe est deja dans le store
   ipcMain.on('has-team', function (event, data) {
     var idTeam = store.get('teamID');
     if(!idTeam) {
@@ -63,40 +64,45 @@ async function createMainWindow() {
     }
     mainWindow.webContents.send('responce-has-team', idTeam);
   });
-
+  // demande depuis la template 
   ipcMain.on('request-team', function(event, data) {
       var error = 'no error';
       var id = data.id;
-      if(isNaN(id)) {
+      if(isNaN(id)) { // verifie que id est un nombre sinon message d'erreur
         error = 'id is not number';
-        var responce = {error : error};
-        mainWindow.webContents.send('responce-team', responce);
+        var responce = {error : error}; 
+        mainWindow.webContents.send('responce-team', responce); // envoie de la reponse vers le rendu
       } else {
+        // DEV ENV ONLY
         sql.query("SELECT * FROM Message JOIN course c on Message.course_id = c.course_id WHERE c.course_id = '" + id + "'", function (error, result) {
             sql.query("SELECT Photo.* FROM Photo JOIN course c on Photo.course_id = c.course_id WHERE c.course_id = '" + id + "'", function (error, photoResult) {
                 var responce = {error : error, messages : result, photos : photoResult};
-                mainWindow.webContents.send('responce-team', responce);
+                mainWindow.webContents.send('responce-team', responce); // envoie de la reponse vers le rendu
             });
         });
+        // Stockage de l'id de l'équipe dans le fichier userData (store)
         store.set('teamID', id);
       }
 
   });
-
+  // evenement bouton ouveture de la page projet 
   ipcMain.on('request-project', function(event, data) {
-      if(!projectWindow) {
+      if(!projectWindow) { // creation de la page si elle n'existe pas 
         createProjectWindow();
       }
+      // affichage de la page
       projectWindow.show();
       projectWindow.focus();
   });
-
+  // evenement 'will-resize' : redim de la page 
   mainWindow.on('will-resize', function (event, bound) {
-    store.set('mainWindowWith', bound.width);
+    // sauvegarde des nouvelles data dans le store
+    store.set('mainWindowWith', bound.width); 
     store.set('mainWindowHeight', bound.height);
   });
 }
 
+// GETTER
 const getMainWindow = () => mainWindow;
 
 module.exports = {
